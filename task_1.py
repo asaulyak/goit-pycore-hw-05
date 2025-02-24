abc2eb0805a6a2b87a54b77c1c2692be8fa4b420
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2025-02-24 20:43:01 +0200

    move fibonaci cache into a closure

--- task_1.py
+++ task_1.py
@@ -1,15 +1,18 @@
-cache = {0: 0, 1: 1}
+def caching_fibonacci():
+    cache = {0: 0, 1: 1}
 
-def caching_fibonacci(n):
-    if not isinstance(n, int):
-        raise ValueError("n must be an integer")
+    def fibonacci(n: int) -> int:
+        if not isinstance(n, int):
+            raise ValueError("n must be an integer")
 
-    p = 0 if n < 0 else n
+        p = 0 if n < 0 else n
 
-    if p in cache:
-        return cache[p]
-    else:
-        result = caching_fibonacci(p - 1) + caching_fibonacci(p - 2)
-        cache[p] = result
+        if p in cache:
+            return cache[p]
+        else:
+            result = fibonacci(p - 1) + fibonacci(p - 2)
+            cache[p] = result
 
-        return result
+            return result
+
+    return fibonacci
